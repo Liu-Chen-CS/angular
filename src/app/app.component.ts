02f517bf30fbda4b10f2ca60df03c7cef789758d
Liu-Chen-CS <liu.chenbusiness@gmail.com>
2024-06-26 20:40:00 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,64 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {NgForOf, NgIf, NgStyle} from "@angular/common";
+
+type Person = {
+  name: string,
+  age: number,
+};
+
+type LoadActiveType = {
+  backgroundColor: string,
+  color:string,
+  borderColor:string,
+}
+
+type noMoreStyleType = {
+  color: string,
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgForOf, NgIf, NgStyle],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular';
+  title: string = 'angular';
+  name: string = "Liu Chen";
+  age: number = 28;
+  list: Person[] = [
+    {
+      name: "Liu Chen",
+      age: 28,
+    },
+    {
+      name: "Lisa Guillard",
+      age: 18,
+    },
+    {
+      name: "BBC",
+      age: 32,
+    }];
+  hasMore:boolean = true;
+  LoadableStyle:LoadActiveType = {
+    backgroundColor: "#383",
+    color: "#fff",
+    borderColor: "#252",
+  };
+  noMoreStyle:noMoreStyleType = {
+    color: "aqua",
+  }
+
+  increment(): void {
+    this.age++;
+  }
+
+  decrement(): void {
+    this.age--;
+  }
+  loadMore():void{
+    this.hasMore = false;
+  }
 }
